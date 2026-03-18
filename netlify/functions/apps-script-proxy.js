9aa3d0871f9c2f18370dc7af7f034e62c0481a8e
adamhiryouorder-cyber <adam.hir.youorder@gmail.com>
2026-03-18 12:53:19 +0100

    Update apps-script-proxy.js

--- netlify/functions/apps-script-proxy.js
+++ netlify/functions/apps-script-proxy.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   try {
-    if (!["GET", "POST"].includes(event.httpMethod)) {
+    if (event.httpMethod !== "POST") {
       return {
         statusCode: 405,
         headers: { "Content-Type": "application/json" },
@@ -17,33 +17,20 @@ exports.handler = async (event) => {
       };
     }
 
-    let response;
+    const body = JSON.parse(event.body || "{}");
 
-    if (event.httpMethod === "GET") {
-      const qs = new URLSearchParams(event.queryStringParameters || {}).toString();
-      const url = qs ? `${APPS_SCRIPT_URL}?${qs}` : APPS_SCRIPT_URL;
-
-      response = await fetch(url, {
-        method: "GET",
-        redirect: "follow"
-      });
-    } else {
-      const body = JSON.parse(event.body || "{}");
-
-      response = await fetch(APPS_SCRIPT_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "text/plain;charset=utf-8"
-        },
-        body: JSON.stringify(body),
-        redirect: "follow"
-      });
-    }
+    const response = await fetch(APPS_SCRIPT_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain;charset=utf-8"
+      },
+      body: JSON.stringify(body)
+    });
 
     const text = await response.text();
 
     return {
-      statusCode: response.status,
+      statusCode: 200,
       headers: { "Content-Type": "application/json" },
       body: text
     };
